ea62f1faf05e9160d552004ec2d15d51b4cbec28
Barbora Herynková <b.herynkova@gmail.com>
2022-10-03 21:32:19 +0200

    potreba spravit open carousel

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const carousel = document.querySelector(".carousel-stranka");
+const carouselClose = document.querySelector(".zavrit");
 const carouselPozadi = document.querySelector(".carousel-pozadi");
-const foto = document.querySelectorAll(".foto")
+const foto = document.querySelectorAll(".foto");
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerMenuButton = document.querySelector(".menu-img");
@@ -21,11 +22,13 @@ const openHamburgerMenu = () => {
   hamburgerMenu.style.display = "block";
 }
 
-carouselPozadi.addEventListener("click", closeCarousel);
-let pocetFotek = foto.length;
-for (let i = 0; i <= pocetFotek; i++) {
-  foto[i].addEventListener("click", openCarousel)
-}
-
 hamburgerMenuButton.addEventListener("click", openHamburgerMenu);
 hamburgerMenu.addEventListener("click", closeHamburgerMenu);
+
+carouselPozadi.addEventListener("click", closeCarousel);
+carouselClose.addEventListener("click", closeCarousel);
+
+let pocetFotek = foto.length;
+for (let i = 0; i <= pocetFotek; i++) {
+  foto[i].addEventListener("click", openCarousel);
+}
