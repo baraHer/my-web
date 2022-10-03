d09fd0b670337de5e57473429652bdd2e9e2c89f
Barbora Herynková <b.herynkova@gmail.com>
2022-10-03 21:12:32 +0200

    zaklad hamburger menu - potreba nastylovat

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const carousel = document.querySelector(".carousel-stranka");
 const carouselPozadi = document.querySelector(".carousel-pozadi");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenuButton = document.querySelector(".menu-img");
 
-const closeCarosel = () => {
+const closeCarousel = () => {
   carousel.style.display = "none";
 }
 
-carouselPozadi.addEventListener("click", closeCarosel);
+const openCarousel = () => {
+  carousel.style.display = "grid";
+}
+
+const closeHamburgerMenu = () => {
+  hamburgerMenu.style.display = "none";
+}
+
+const openHamburgerMenu = () => {
+  hamburgerMenu.style.display = "block";
+}
+
+carouselPozadi.addEventListener("click", closeCarousel);
+
+hamburgerMenuButton.addEventListener("click", openHamburgerMenu);
+hamburgerMenu.addEventListener("click", closeHamburgerMenu);
